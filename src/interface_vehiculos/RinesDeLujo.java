15398e0ee03dd49e7ac848bf52a46e19479903a5
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-05 23:43:33 -0500

    [FEATURE] Add RinesDeLujo class

---
+++ src/interface_vehiculos/RinesDeLujo.java
@@ -0,0 +1,19 @@
+package interface_vehiculos;
+
+public class RinesDeLujo implements Vehiculo{
+    private Vehiculo vehiculo;
+
+    public RinesDeLujo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    @Override
+    public Double getPrecio() {
+        return vehiculo.getPrecio()+10.0;
+    }
+
+    @Override
+    public String getAccesorios() {
+        return vehiculo.getAccesorios()+"Rines de lujo";
+    }
+}
